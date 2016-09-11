33e6cdddeccd1eee8e493a48bc5e1f6c4b3611aa
Clint Mario Cleetus <clintmario@gmail.com>
2016-09-11 11:25:47 -0400

    Cosmetic changes in email notifications.

--- app/Modules/Home/Views/Fortuna/email-content.blade.php
+++ app/Modules/Home/Views/Fortuna/email-content.blade.php
@@ -1,5 +1,5 @@
 <div style="margin:20px 0;">
-    <table style="border-collapse: collapse;" cellpadding="5">
+    <table style="border-collapse: collapse;" cellpadding="5" align="center">
         <tr>
             <th colspan="2" style="background:#ccc; border: 1px solid gray;"><strong>Contact Details</strong></th>
         </tr>
@@ -21,7 +21,7 @@
 
         <tr>
             <td style="background:#99B7D4; border: 1px solid gray;"><strong>Message</strong></td>
-            <td style="border: 1px solid gray;">{{ $contact['message'] }}</td>
+            <td style="border: 1px solid gray;">{!! nl2br(e($contact['message'])) !!}</td>
         </tr>
     </table>
 </div>
